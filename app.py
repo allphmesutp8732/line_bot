0e220c796e65f56ce2d365db5818dc47a6085374
Wayne Lin <wayne23726489@gmail.com>
2020-02-19 18:24:54 +0800

    add runtime file

--- app.py
+++ app.py
@@ -12,8 +12,8 @@ from linebot.models import (
 
 app = Flask(__name__)
 
-line_bot_api = LineBotApi('YOUR_CHANNEL_ACCESS_TOKEN')
-handler = WebhookHandler('YOUR_CHANNEL_SECRET')
+line_bot_api = LineBotApi('qpWMu2KuLbVUwhLGyqiZKc0AVqoZwPKGJNw0aMjxAsmR9E1ZQ6h/7lhsQAlSN24Yf5tDBZ7xy6Bb7pryoXmD+6R7L4tor3O8csgz+jXW+4p2z68SvYq3sz3NY2J7gkVnLE1XiPclzMUmjAj3moFfmAdB04t89/1O/w1cDnyilFU=')
+handler = WebhookHandler('e262791ee0d485493cddece097761a9d')
 
 
 @app.route("/callback", methods=['POST'])
