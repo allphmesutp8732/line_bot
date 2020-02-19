346063c1b2cfd6213d5b1d6945c058f3b79c7284
Wayne Lin <wayne23726489@gmail.com>
2020-02-20 00:30:55 +0800

    updated.

--- app.py
+++ app.py
@@ -49,7 +49,7 @@ def handle_message(event):
     #     r = str(now.hour) + ":" + str(now.minute) + ":" + str(now.second)
     line_bot_api.reply_message(
         event.reply_token,
-        TextSendMessage(text=r))
+        TextSendMessage(text=event.message.text))
 
 
 if __name__ == "__main__":
